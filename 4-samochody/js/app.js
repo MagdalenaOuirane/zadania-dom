691bfa641c9ac888a08c61c874800732f434c628
unknown <magdalenaouirane@gmail.com>
2020-11-29 18:27:05 +0100

    task done

---
+++ 4-samochody/js/app.js
@@ -0,0 +1,19 @@
+const btns = document.querySelectorAll('.car-toggle-detail');
+
+for (let btn of btns) {
+    btn.addEventListener("click", e => {
+        const carDiv = btn.closest(".car");
+        const carDetail = carDiv.querySelector(".car-detail");
+
+        carDiv.classList.toggle("car-show-detail");
+
+        if (carDiv.classList.contains("car-show-detail")) {
+            carDetail.style.display = "block"; //pokazuję
+            btn.innerText = "schowaj detale";
+        } else {
+            carDetail.style.display = ""; //ukrywam
+            btn.innerText = "pokaż detale";
+        }
+    })
+}
+
